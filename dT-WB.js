b96f972419f0514950cedea96e99215ca1408319
bjeelaa <belahoutman@gmail.com>
2024-03-16 22:49:17 +0100

    done

--- dT-WB.js
+++ dT-WB.js
@@ -8,19 +8,19 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
     console.log(pako);
     function decodeData(encodedData) {
-        return pako.ungzip(atob(encodedData),{"to":"string"});
+        return pako.ungzip(atob(encodedData), { "to": "string" });
     }
 
     // Intercept XMLHttpRequest
     var origOpen = XMLHttpRequest.prototype.open;
-    XMLHttpRequest.prototype.open = function(method, url) {
+    XMLHttpRequest.prototype.open = function (method, url) {
         console.log('HTTP Request:', method, url);
-        this.addEventListener('load', function() {
+        this.addEventListener('load', function () {
             console.log('HTTP Response:', this.status, this.responseText);
             var rawResponseBody = this.responseText;
             var parsedBody = JSON.parse(rawResponseBody);
@@ -33,8 +33,44 @@
             var items = parsedDefinition.items;
             console.log(items);
             var keys = Object.keys(items);
-            console.log(keys[0]);
-            
+            console.log(keys);
+
+            for (var i = 0; i < keys.length; i++) {
+                if (keys[i].length != 36) {
+                    keys.splice(i, i);
+                };
+            };
+            console.log(keys);
+
+            var htmlContent = `
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Oplossingen</title>
+</head>
+<body>
+  <h1>De oplossingen voor je oefening:</h1>
+`;
+
+            for (var k = 0; k < keys.length; k++) {
+                var key = keys[k];
+                var options = items[key].options;
+                for (var j = 0; j < options.length; j++){
+                    if(options[j].correct){
+                        var answer = options[j].text;
+                        break;
+                    };
+                };
+                htmlContent = htmlContent + '  <p>' + (k+1).toString() + '. ' + answer + `</p>
+`;
+            };
+            htmlContent = htmlContent + `</body>
+</html>`;
+            console.log(htmlContent);
+            var blob = new Blob([htmlContent], { type: 'text/html' });
+            var url = URL.createObjectURL(blob);
+            window.open(url, '_blank', 'width=400,height=400');
+            URL.revokeObjectURL(url);
         });
         origOpen.apply(this, arguments);
     };
